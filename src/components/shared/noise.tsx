a91750d33548851f135fa028e01833164c828c28
wafley <alwafielgin@gmail.com>
2026-06-01 16:08:30 +0700

    style: reduce noise component opacity and base frequency for a more subtle texture

--- src/components/shared/noise.tsx
+++ src/components/shared/noise.tsx
@@ -5,8 +5,8 @@ interface NoiseProps {
 }
 
 export function Noise({
-  opacity = 0.1,
-  baseFrequency = 1.3,
+  opacity = 0.02,
+  baseFrequency = 0.3,
   numOctaves = 3,
 }: NoiseProps) {
   const svgNoise = `data:image/svg+xml,%3Csvg viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='${baseFrequency}' numOctaves='${numOctaves}' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E`;
