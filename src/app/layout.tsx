4401a49ac07e36dbd591e4034ffe37f31adbf9c2
wafley <alwafielgin@gmail.com>
2026-06-01 08:59:57 +0700

    fix: update default theme to dark and sync package-lock.json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       <body className="flex min-h-full flex-col">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
