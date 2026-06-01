e8740b814dbaf4539e217246f37aa3a710f0b5a4
wafley <alwafielgin@gmail.com>
2026-06-01 13:37:00 +0700

    feat: add dynamic page title to home route using site configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import { HeroSection } from "@/components/sections/HeroSection";
+import { SITE_CONFIG } from "@/constants";
+
+export const metadata = {
+  title: `Home - ${SITE_CONFIG.name}`,
+};
 
 export default function Home() {
   return (
